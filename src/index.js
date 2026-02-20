f94afe2ec163977f2adf7366ca821f1c7ad3690a
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-20 08:59:29 -0300

    callback

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-fs.readFile(link, 'utf-8')
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    console.log(texto)
+})
 
 console.log(link)
