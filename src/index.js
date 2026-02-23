0c31da743a86b35cb37708259c30c77846a6a5e0
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 08:12:15 -0300

    erro

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function quebraEmParagrafos(texto){
 }
 
 function limpaPalavras(){
-                return palavra.replace('(', ' ');
+                return palavra.replace();
 }
 
 function verificaPalavrasDuplicadas(texto){
